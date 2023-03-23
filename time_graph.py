137ce3a1f70befee41687ac195fbe16a7dbc5066
vibhu01 <vibhumrajt.developer@gmail.com>
2023-03-24 01:29:08 +0530

    hopefully final script

--- time_graph.py
+++ time_graph.py
@@ -6,7 +6,7 @@ from polygenerator import (
     random_polygon,
 )
 
-c = list(10*x for x in range(1,10)) + list(100*x for x in range(1,3))
+c = list(range(20,200,10))
 def show(dict):
     print("N : Decomp time,Merge Time,Total")
     for t in c:
@@ -36,12 +36,14 @@ df = pd.DataFrame(dict).T
 df[0].plot(label="Decomp Time")
 df[1].plot(label="Merge Time")
 plt.figure(1)
-plt.title("Variation of Time Taken")
+plt.title("Decomposition Time & Merge Time")
 plt.ylabel("Time(s)")
 plt.xlabel("Number of vertices")
+plt.savefig("Total_time.png")
 plt.figure(2)
 df[1].plot(color='orange',label="Merge Time")
-plt.title("Variation of Time Taken")
+plt.title("Merge Time")
 plt.ylabel("Time(s)")
-plt.show()
+plt.savefig("Merge_time.png")
+# plt.show()
 show(dict)
